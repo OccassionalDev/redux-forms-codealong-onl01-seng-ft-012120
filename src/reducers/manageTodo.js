7f5f1c9e05a94fd9c688db4cc946094d260cc239
Erica Schimmel <ericaschimmel01@gmail.com>
2020-06-11 02:56:45 +0000

    Automatically backed up by Learn

--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -2,7 +2,8 @@ export default function manageTodo(state = {
   todos: [],
 }, action) {
 
-  console.log("reducer received this action:", action);
-
-  return state;
+  switch(action.type) {
+    case 'ADD_TODO':
+      
+  }
 }
